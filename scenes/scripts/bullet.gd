f89e5add2201268b59de204222e3bb1169e06333
sayed-qubadi <>
2025-03-05 11:22:50 -0800

    Update bullet.gd
    added function to calculate the angle between mouse pointer and bullet_sprite. Need to create/find player sprite.

--- scenes/scripts/bullet.gd
+++ scenes/scripts/bullet.gd
@@ -5,7 +5,13 @@ var spawnPos : Vector2
 var dir: int
 var speed = 1000
 
-
+func _process(delta):
+	#get mouse position in world space
+	var target_pos = get_global_mouse_position()
+	#calculate the direction vector from the player to the target
+	var direction = target_pos - bullet_sprite.global_position
+	#set the bullet's rotation to the angle of that vector
+	bullet_sprite.rotation = direction.angle()
 #work on collison
 func _ready():
 	position = spawnPos
@@ -18,6 +24,6 @@ func _on_body_entered(body):
 	if body is TileMapLayer || body is RigidBody2D:
 		queue_free()
 	else:
-		body.hit(0)
+		body.hit()
 		print(body.health)
 		queue_free()
