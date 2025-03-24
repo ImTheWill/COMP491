473e59d197d148dd48d443b32a7c885f7336ccd6
spencerCrowell <spencer.crowell.694@my.csun.edu>
2025-03-24 11:16:38 -0700

    almost complete boss enemy
    boss enemy is almost done. all the logic is complete, the player taking damage is a little buggy, will fix that soon

--- scripts/player.gd
+++ scripts/player.gd
@@ -223,7 +223,7 @@ func shoot():
 	get_tree().root.add_child(new_bullet)
 
 func hit(direction):
-	velocity.x += 300 * direction
+#	velocity.x += 300 * direction
 	move_and_slide()
 	player_sprite.play("hurt")
 	await get_tree().create_timer(0.5).timeout
