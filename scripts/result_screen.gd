5b8a1ba4465bb8a9a842f4b3de9a99890a51fc1f
HONGYANGGAN <kam667755@gmail.com>
2025-03-29 18:32:21 -0700

    feat: added new elements to achievement system/result screen.

--- scripts/result_screen.gd
+++ scripts/result_screen.gd
@@ -2,7 +2,11 @@ extends Control
 
 
 @onready var win_text = $Panel/WinContainer/WinText
-@onready var achievement_text = $Panel/VBoxContainer/AchievementText
+@onready var achievement_text = $Panel/VBoxContainer/AchievementRow1/AchievementText
+@onready var star_image = $Panel/VBoxContainer/AchievementRow1/StarImage
+
+var star_frame_texture = preload("res://assets/Achievement UI/2.png")
+var star_full_texture = preload("res://assets/Achievement UI/1.png")
 
 
 func _ready():
@@ -13,5 +17,11 @@ func show_simple_result():
 	
 	print("show_simple_result() called")
 	print("AchievementText node:", achievement_text)
-	achievement_text.text = "Achievement unlocked: Defeat the boss"
+	achievement_text.text = " Defeat boss: "
+	
+	star_image.texture = star_frame_texture
+	
 	visible = true
+	
+	await get_tree().create_timer(1.2).timeout
+	star_image.texture = star_full_texture
