4f7fb92e6cdfc5c86db989471e892aa085440efd
sayed-qubadi <sayed.qubadi.761@my.csun.edu>
2025-04-23 18:46:00 -0700

    merged 360-aim to main
    manually.

--- scripts/turrent_enemy.gd
+++ scripts/turrent_enemy.gd
@@ -48,7 +48,11 @@ func _physics_process(_delta):
 
 func shoot():
 	var new_bullet = BULLET.instantiate()
-	new_bullet.dir = -1 if turrent_sprite.flip_h else 1
+	#new_bullet.direction = -1 if turrent_sprite.flip_h else 1
+	
+	var direction = Vector2.LEFT if turrent_sprite.flip_h else Vector2.RIGHT
+	new_bullet.set_direction(direction)
+
 	new_bullet.spawnPos = bullet_start.global_position 
 	new_bullet.speed = 500
 	get_tree().root.add_child(new_bullet)
