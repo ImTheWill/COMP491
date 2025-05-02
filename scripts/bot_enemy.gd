978047d6c7e06c3fc62589a0de8cd07aebc1fe73
sayed-qubadi <sayed.qubadi.761@my.csun.edu>
2025-05-02 10:27:03 -0700

    work in progress
    bot enemy to chase player and aim at player 0-180 degrees

--- scripts/bot_enemy.gd
+++ scripts/bot_enemy.gd
@@ -4,20 +4,33 @@ signal enemy_defeated
 @onready var bot_player_ray = $botPlayerRay
 @onready var bot_floor_ray = $botFloorRay
 @onready var enemy_health_bar = $EnemyHealthBar
-@onready var bullet_start = $bulletStart
+# @onready var bullet_start = $bulletStart
+@export var bullet_start: Node2D
 @onready var bot_sprite = $botSprite
 @onready var timer = $Timer
-@onready var BULLET = load("res://scenes/player/bullet.tscn")
+# @onready var BULLET = load("res://scenes/player/bullet.tscn")
+@export var BULLET: PackedScene
 @onready var area2d = $Area2D
+@export var move_speed = 150
+@export var shoot_cooldown = 0.5  # Seconds between shots
+@onready var turret_sprite = $TurretSprite
+
 
 var speed = 60
 var facing_right = false
 var health = 100
 var points_per_kill = 100
+var player: Node2D
+var can_shoot = true
 
 func _ready():
 	bot_sprite.play("patrol")
 	add_to_group("Enemy")
+	timer.wait_time = shoot_cooldown
+	timer.one_shot = true
+	player = get_parent().get_node_or_null("Player")  # Adjust path if necessary
+	if not player:
+		print("Player not found in scene!")
 	
 func _physics_process(delta):
 	var botPlayerRay = bot_player_ray.get_collider()
@@ -39,9 +52,27 @@ func _physics_process(delta):
 		velocity.y += get_gravity().y *delta
 	if !bot_floor_ray.is_colliding() && is_on_floor():
 		flip()
-		
+	
+	if player:
+		# Move towards player
+		var direction_to_player = (player.global_position - global_position).normalized()
+		velocity = direction_to_player * move_speed
+		move_and_slide()
+
+		# Aim turret at player
+		var to_player = player.global_position - bullet_start.global_position
+		turret_sprite.rotation = to_player.angle()
+
+		# Shoot if player is in front (0-180 degrees)
+		var facing_vector = Vector2.RIGHT.rotated(rotation)
+		var angle_to_player = facing_vector.angle_to(to_player.normalized())
+
+		if abs(angle_to_player) < PI / 2 and can_shoot:  # Only shoot if player is roughly in front
+			shoot()
+
 	velocity.x = speed
 	move_and_slide()
+	
 
 
 
@@ -63,16 +94,30 @@ func flip():
 	else:
 		speed = abs(speed)
 func shoot():
+	if not BULLET:
+		return
+
 	print("has shot")
 	var new_bullet = BULLET.instantiate()
+	new_bullet.spawnPos = bullet_start.global_position
+
+	# Calculate direction to player
+	var shoot_dir = (player.global_position - bullet_start.global_position).normalized()
+	new_bullet.set_direction(shoot_dir)  # Assuming bullet.gd has set_direction()
+
+	#var direction = Vector2.LEFT if bot_sprite.flip_h else Vector2.RIGHT
+	#new_bullet.set_direction(direction)
 	
-	var direction = Vector2.LEFT if bot_sprite.flip_h else Vector2.RIGHT
-	new_bullet.set_direction(direction)
-	
-	new_bullet.spawnPos = bullet_start.global_position 
+	# new_bullet.spawnPos = bullet_start.global_position 
 	new_bullet.speed = 500
 	get_tree().root.add_child(new_bullet)
+
+	can_shoot = false
 	timer.start()
+
+func _on_timer_timeout():
+	can_shoot = true
+
 
 func hit(_direction):
 		health -= 10
